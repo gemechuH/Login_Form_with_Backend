c17947e5ecd6fafb84d5d45a714059be4b0e4116
gemechuH <gemechuhunduma100@gmail.com>
2025-05-08 01:26:43 -0800

    why it is not working

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,6 +1,13 @@
-//went to use baseurl to avoid typing the same url over and over again http://localhost:5000
-const BASE_URL = "https://ethio-parent-school-backend.vercel.app";
+const BASE_URL = import.meta.env.PROD
+  ? "https://ethio-parent-school-backend.vercel.app"
+  : "http://localhost:5000";
+
+export const fetchConfig = {
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  mode: "cors",
+};
 
 export default BASE_URL;
-
-
